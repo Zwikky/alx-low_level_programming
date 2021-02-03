3ca4b43f3016a30f82701bf2390b9cf2c02c76ee
Zwakele Mkhatshwa <zwikky@gmail.com>
2021-02-03 21:54:43 +0000

    6. Size dot C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - Main function to print various data type sizes
+*
+*Return: just a 0
+*/
+
+int main(void)
+{
+char a;
+int b;
+long c;
+long long d;
+float e;
+
+printf("Size of a char: %d byte(s)\n", (int) sizeof(a));
+printf("Size of an int: %d byte(s)\n", (int) sizeof(b));
+printf("Size of a long int: %d byte(s)\n", (int) sizeof(c));
+printf("Size of a long long int: %d byte(s)\n", (int) sizeof(d));
+printf("Size of a float: %d byte(s)\n", (int) sizeof(e));
+}
