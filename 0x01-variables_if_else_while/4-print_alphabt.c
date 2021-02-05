af75916bd754b1287ee53d989b408f3404208919
Zwakele <zwikky@gmail.com>
2021-02-05 20:17:24 +0000

    4. Print Aplphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main Function --- prints the alphabet in lowercase
+ *
+ * Return: just a 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
