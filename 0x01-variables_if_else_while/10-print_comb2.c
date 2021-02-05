07cdbefbfd331a31167df1592759a0d853a1a42e
Zwakele <zwikky@gmail.com>
2021-02-05 21:00:08 +0000

    10.

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Main Function --- prints the numbers from 00 to 99
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '9' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
